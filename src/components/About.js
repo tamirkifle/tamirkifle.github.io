843a7e2b356328e75b4f87dde7b584091b205f2a
Tamir Yirga <tkifle@gmail.com>
2025-03-12 17:29:45 -0700

    Made About more friendly

--- src/components/About.js
+++ src/components/About.js
@@ -6,20 +6,21 @@ export default function About() {
   useEffect(() => {
     const container = ".tagcloud";
     const texts = [
-      "Angular",
+      "AWS",
       "React",
       "Javascript",
-      "Azure",
       "Blockchain",
+      "Redis",
+      "RabbitMq",
       "Machine Learning",
       "Java",
       "Python",
-      "Google Cloud",
-      "Firebase",
       "Firestore",
       "HTML",
       "CSS",
-      "Adobe Commerce"
+      "Adobe Commerce",
+      "Azure",
+      "Google Cloud"
     ];
 
     const options = { radius: 200, maxSpeed: "fast", initSpeed: "fast" };
@@ -32,19 +33,18 @@ export default function About() {
         <div className="text-container flow-content">
           <div className="about--intro flow-content">
             <h2 className="about--intro--name">
-              Hi, <br />
-              I'm <span>Tamir.</span>
+              👋 I'm <span>Tamir!</span>
             </h2>
             <p className="about--intro--subtitle">
-              I am a <span className="highlight">full-stack</span>{" "}
+              A <span className="highlight">full-stack</span>{" "}
               <span className="highlight">developer</span> based in{" "}
               <span className="highlight no-wrap">Seattle</span>,{" "}
               <span className="highlight">Washington</span>
             </p>
           </div>
           <p className="about--desc">
-            I build web applications focusing on quality and maintainabilty.
-            Feel free to take a look at some of my work below.
+            Passionate about building scalable, distributed systems with a focus on quality, maintainability.
+            <br /> Feel free to take a look at some of my work below.
           </p>
         </div>
         <img
