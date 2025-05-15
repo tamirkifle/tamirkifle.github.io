1e25deb37f7cc665e402e708d89d063d73ecaba6
Tamir Yirga <tkifle@gmail.com>
2025-05-14 19:02:56 -0700

    Used separate close and open modal functions; Added esc to close modal

--- src/components/utility/Modal.js
+++ src/components/utility/Modal.js
@@ -2,21 +2,34 @@ import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
 const modalRoot = document.getElementById("modal");
-const Modal = ({ children, toggleModal }) => {
+const Modal = ({ children, closeModal }) => {
   const elRef = useRef(null);
   if (!elRef.current) {
     elRef.current = document.createElement("div");
     elRef.current.classList.add("modal--container");
   }
 
+  const handleEscKeyPress = (event) => {
+    if (event.key === 'Escape') {
+      closeModal()
+    }
+  };
+
   useEffect(() => {
     modalRoot.appendChild(elRef.current);
     return () => modalRoot.removeChild(elRef.current);
   }, []);
 
+  useEffect(() => {
+    document.addEventListener('keydown', handleEscKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleEscKeyPress);
+    };
+  }, []);
+
   return createPortal(
     <>
-      <div className="modal--overlay" onClick={toggleModal}></div>
+      <div className="modal--overlay" onClick={closeModal}></div>
       <div className="modal--box">{children}</div>
     </>,
     elRef.current
